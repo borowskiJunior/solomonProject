81bbaf4d68c33d76104b79be509fbfcb7909616a
Max Borowski <maxborowskimusic@gmail.com>
2024-03-26 13:18:48 +0300

    v0.1

---
+++ src/main/java/com/solomon/backend/solomonproject/SolomonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.solomon.backend.solomonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolomonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolomonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solomon/backend/solomonproject/SolomonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomon.backend.solomonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolomonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
