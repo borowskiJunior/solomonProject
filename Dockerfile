a6c3f91703b1e02fcd50f0c3153d3453bc89d467
Max Borowski <maxborowskimusic@gmail.com>
2024-05-05 22:49:38 +0300

    v0.2.8 (cors update)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM  openjdk:17-oracle
 EXPOSE 8080
 WORKDIR /usr/solomon/src
-COPY build/libs/solomonProject-0.0.1-SNAPSHOT.war /usr/solomon/src/
+COPY build/libs/solomonProject-0.0.1-SNAPSHOT.jar /usr/solomon/src/
 
-CMD ["java", "-jar", "solomonProject-0.0.1-SNAPSHOT.war"]
+CMD ["java", "-jar", "solomonProject-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/solomon/backend/solomonproject/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.solomon.backend.solomonproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author Max Borowski
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
